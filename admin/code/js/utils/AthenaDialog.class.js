ed65c812880f30624885c342764b6465c531ff56
Mike Pritchard <mike@adastrasystems.com>
2010-09-11 10:24:27 -0600

    dialog background set to black, and opacity = 0.3

--- admin/code/js/utils/AthenaDialog.class.js
+++ admin/code/js/utils/AthenaDialog.class.js
@@ -3,11 +3,11 @@
 */
 var AthenaDialog = {
 
-	// ////////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////////
 
-	showLoading : function(msg){
+    showLoading : function(msg){
 
-/*		
+        /*
 		var txt = "";
 		txt += "<div class='loading_box' style='position:relative; top:45%; width:200px; overflow:hidden' align='center'>";		
 		txt += "    <img  src='"+defines.root_url+"images/loading_spinner.gif'/>";
@@ -21,112 +21,120 @@ var AthenaDialog = {
 */
 
 
-		jQuery('#apollo_dialog').dialog("destroy");
+        $('#apollo_dialog').dialog("destroy");
 				
-		jQuery('#apollo_dialog').html("<div align='center'><img src='"+defines.root_url+"images/spinner.gif'/></div>");
+        $('#apollo_dialog').html("<div align='center'><img src='"+defines.root_url+"images/spinner.gif'/></div>");
 		
-		jQuery('#apollo_dialog').dialog({
-			resizable: true,
-			height:70,
-			width: 250,
-		 closeOnEscape: false,
-			modal: true,
-			title: msg
-		})
+        $('#apollo_dialog').dialog({
+            resizable: true,
+            height:70,
+            width: 250,
+            closeOnEscape: false,
+            modal: true,
+            //overlay: {opacity: 0.1, background: "black"},
+            title: msg
+        })
 		
-	},
+    },
 
-	// ////////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////////
 
-	clearLoading : function(){
-		//$('#apollo_loading_display').html("");
-		//$('#apollo_loading_display').hide();
-		jQuery('#apollo_dialog').dialog("destroy");
-	},
+    clearLoading : function(){
+        //$('#apollo_loading_display').html("");
+        //$('#apollo_loading_display').hide();
+        $('#apollo_dialog').dialog("destroy");
+    },
 
-	// ////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////
 
-	error : function(msg){
+    error : function(msg){
 
-		jQuery('#apollo_dialog').dialog("destroy");
+        $('#apollo_dialog').dialog("destroy");
 				
-		jQuery('#apollo_dialog').html(msg);
+        $('#apollo_dialog').html(msg);
 		
-		jQuery('#apollo_dialog').dialog({resizable: false, height:140, modal: true, title: "Error"});
-	},
+        $('#apollo_dialog').dialog({
+            resizable: false,
+            height:140,
+            modal: true,
+            title: "Error"
+        });
+    },
 	
-	// ////////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////////
 
-	/**
+    /**
 	* Display a info message to the user
 	*/
-	message : function(msg){
-		//alert(msg);
-	},
+    message : function(msg){
+    //alert(msg);
+    },
 	
-	// ////////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////////
 
-	/**
+    /**
 	* Display an alert to the user
 	*/
-	alert : function(msg, msgTitle){
+    alert : function(msg, msgTitle){
 		
-		if (msgTitle == undefined){
-			msgTitle = "Message";
-		}
+        if (msgTitle == undefined){
+            msgTitle = "Message";
+        }
 		
-		jQuery('#apollo_dialog').dialog("destroy");
+        $('#apollo_dialog').dialog("destroy");
 				
-		jQuery('#apollo_dialog').html(msg);
+        $('#apollo_dialog').html(msg);
 		
-		jQuery('#apollo_dialog').dialog({
-			resizable: false,
-//			height:140,
-			modal: true,
-			title: msgTitle,
-			buttons: {
-				Ok: function() {
-					jQuery(this).dialog('close');
-				}
-			}
-		})
-	},
+        $('#apollo_dialog').dialog({
+            resizable: false,
+            //			height:140,
+            modal: true,
+            title: msgTitle,
+            buttons: {
+                Ok: function() {
+                    $(this).dialog('close');
+                }
+            }
+        })
+    },
 	
-	// ////////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////////
 
-	confirm : function(msg, onOKCallback, onCancelCallback){
+    confirm : function(msg, onOKCallback, onCancelCallback){
 				
-		jQuery('#apollo_dialog').dialog("destroy");
+        $('#apollo_dialog').dialog("destroy");
 				
-		jQuery('#apollo_dialog').html(msg);
+        $('#apollo_dialog').html(msg);
 		
-		jQuery('#apollo_dialog').dialog({
-			resizable: false,
-			height:140,
-			modal: true,
-			title: "Confirm",
-			buttons: {
-				Cancel: function() {
-					jQuery(this).dialog('close'); if (onCancelCallback) onCancelCallback();
-				},
-				Ok: function() {
-					jQuery(this).dialog('close'); if (onOKCallback) onOKCallback();
-				}
-			}
-		})
+        $('#apollo_dialog').dialog({
+            resizable: false,
+            height:140,
+            modal: true,
+            title: "Confirm",
+            buttons: {
+                Cancel: function() {
+                    $(this).dialog('close');
+                    if (onCancelCallback) onCancelCallback();
+                },
+                Ok: function() {
+                    $(this).dialog('close');
+                    if (onOKCallback) onOKCallback();
+                }
+            }
+        })
 		
-	},
+    },
 		
-	// ////////////////////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////////////////////
 	
-	showAjaxError : function(ret){
+    showAjaxError : function(ret){
 		
-		if (ret.data != undefined){
-			alert(ret.result + '\n' + ret.data);
-		}
-		else {
-			alert(ret.data);
-		}
+        if (ret.data != undefined){
+            alert(ret.result + '\n' + ret.data);
+        }
+        else {
+            alert(ret.data);
+        }
 		
-	}
+    }
 }
