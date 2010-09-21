f8db4cf6d77592917dec2c9a0643af28e5bbdc7d
Mike Pritchard <mike@adastrasystems.com>
2010-09-21 13:15:30 -0600

    sync

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 require_once("admin/code/php/setup.php");
 
 //Logger::echoLog();
-
 //$domain = strtolower($_SERVER['HTTP_HOST']);
 //$page = strtolower(basename($_SERVER['REQUEST_URI']));
 //$path = strtolower(dirname($_SERVER['REQUEST_URI']));
@@ -17,59 +16,75 @@ $parts = parse_url($url);
 // ?year=xxxxx - displays all blog posts for this year
 // ?year=xxxxx&month=xxxxxx - displays all blog posts for this year and month
 // ?page=xxxxx - displays a specific blog page, used for older/newer link
-$tag = ''; $category = ''; $query_string = ''; $month = ''; $year = ''; $page_no = '';
+$tag = '';
+$category = '';
+$query_string = '';
+$month = '';
+$year = '';
+$page_no = '';
 
 $domain = $parts['host'];
 $page = strtolower(basename($parts['path']));
 $path = strtolower(dirname($parts['path']));
-if ($path != "/") $path .= "/";
+if ($path != "/")
+    $path .= "/";
 
-if (isset($parts['query'])){
-	$query_string = $parts['query'];
-	parse_str($parts['query']); // Read the query string into variables
+if (isset($parts['query'])) {
+    $query_string = $parts['query'];
+    parse_str($parts['query']); // Read the query string into variables
 }
-$ext = substr(strrchr($page,'.'),1);
+$ext = substr(strrchr($page, '.'), 1);
 
 //Logger::debug(">>>> Request page: $page Ext: $ext Full request: " . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 //Logger::debug(">>>> Domain: $domain Page: $page Path: $path ");
 //Logger::debug(">>>> Tag: $tag Category: $category Month: $month Year: $year");
 
+PageManager::init();
 
-if ($page == '' || (($ext == 'html') || ($ext == 'htm') || ($ext == 'php'))){
-	
-	// Strip www..
-	$domain = str_replace('www.','',$domain);
-	
-	//Logger::debug("Domain: $domain");
-	//Logger::debug("Page: $page");
-	
-	PageManager::init($page, $path, $tag, $category, $month, $year, $page_no);
-	
-	// Log the page view if this is a real page
-	PageViewsTable::logView(PageManager::$site_id, PageManager::$page_id, $page, $path, $query_string);
-	
-	/*
-	if (PageManager::$site_id){
-		Logger::debug("Site ID: ".PageManager::$site_id." Theme ID: ".PageManager::$theme_id." Theme URL: ".PageManager::$theme_url_root);
-	}
-	else {
-		Logger::echoLog();
-		Logger::fatal("Domain does not exist!");
-	}
-	*/
-	
-	// Echo header
-	require_once(PageManager::$theme_file_root . 'header.php');
-	
-	// Get the contents..
-	require_once(PageManager::$theme_file_root . 'page_templates/' . PageManager::$template_filename);
-	
-	// Echo footer
-	require_once(PageManager::$theme_file_root . 'footer.php');
+if ($page == '' || (($ext == 'html') || ($ext == 'htm') || ($ext == 'php'))) {
 
+    PageManager::load($page, $path, $tag, $category, $month, $year, $page_no);
+
+    // Log the page view if this is a real page
+    PageViewsTable::logView(PageManager::$site_id, PageManager::$page_id, $page, $path, $query_string);
+
+    // Echo header
+    require_once(PageManager::$theme_file_root . 'header.php');
+
+    // Get the contents..
+    require_once(PageManager::$theme_file_root . 'page_templates/' . PageManager::$template_filename);
+
+    // Echo footer
+    require_once(PageManager::$theme_file_root . 'footer.php');
 }
 else {
-	echo "Oops, 404 ERROR!";
-}
 
+    // Try to get a site id, if we can fint one then hit its 404 page!
+    $site = SitesTable::getSiteFromDomain($domain);
+
+    if (isset($site)){
+
+        $site_id = $site['id'];
+        $page404 = PageManager::$theme_file_root . '404.php';
+
+        if (file_exists($page404) ){
+            
+            // Echo header
+            require_once(PageManager::$theme_file_root . 'header.php');
+
+            // Get the contents..
+            require_once($page404);
+
+            // Echo footer
+            require_once(PageManager::$theme_file_root . 'footer.php');
+            
+        }
+        else {
+            echo "Oops, 404 ERROR!";
+        }
+    }
+    else {
+        echo "Oops, 404 ERROR!";
+    }
+}
 ?>
