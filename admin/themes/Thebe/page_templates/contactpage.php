a3b1fb13465e823b60a04c95455bc266403b150c
Mike Pritchard <mike@adastrasystems.com>
2010-10-15 11:48:33 -0600

    thebe structure

---
+++ admin/themes/Thebe/page_templates/contactpage.php
@@ -0,0 +1,225 @@
+<?php
+/**
+* @Theme: HollyPacione
+* @Template: Contact Page
+* @Description: Contact Page
+*/
+
+$background_image = PageManager::getMediaURLFromThemePara(205); 
+// Email = 206 
+?>
+
+
+	<div id="content" style="border:none;">
+		
+		<table border="0" cellpadding="0" cellspacing="0" id='contentTable' width="100%" height="100%" style="width:100%; height:100%;background-image: url('<?=$background_image?>')">
+			
+			<tr valign="top" align='left' height="100%">
+
+				<td width="55%" height="100%"> 
+					<!-- Empty -->
+				</td>
+							
+				<td width="45%" height="100%" class='rightCol'>
+					<?php echo PageManager::getCurrentPageContent(); ?>
+					
+					<form id='contactForm' method='post' action='' onsubmit="hpContact.onSubmit(); return false;">
+						<input type="text" class='inputText required_name' id='name' name='name' value="Your Name" onfocus="if (this.value == 'Your Name'){this.value = '';}" onblur='hpContact.onChange()'>
+						<input type="text" class='inputText required_email' id='email' name='email' value="Your E-mail Address" onfocus="if (this.value == 'Your E-mail Address'){this.value = '';}" onblur='hpContact.onChange()'>
+						<input type="text" class='inputText required_phone' id='phone' name='phone' value="Your Phone" onfocus="if (this.value == 'Your Phone'){this.value = '';}" onblur='hpContact.onChange()'>
+						<input type="text" class='inputText' id='location' name='location' value="Event Location" onfocus="if (this.value == 'Event Location'){this.value = '';}" onblur='hpContact.onChange()'>
+						<input type="text" class='inputText datepicker' id='datetime' name='datetime' value="Requested Date">
+						<textarea class='inputTextArea' id='comments' name='comments' onfocus="if (this.value == 'How did you find out about us?'){this.value = '';}" rows="5">How did you find out about us?</textarea>
+						<input type="submit" value="Submit" onclick=""><span class='formMessage'></span>
+						<!-- <button onclick="hpContact.onSubmit()">Submit</button> -->
+					</form>
+				</td>
+				
+			</tr>
+			
+		</table>
+		
+	</div>
+	
+	
+		
+<script type="text/javascript">
+
+// Major version of Flash required
+var requiredMajorVersion = 8;
+
+// Minor version of Flash required
+var requiredMinorVersion = 0;
+
+// Minor version of Flash required
+var requiredRevision = 0;
+
+var hasFlash = DetectFlashVer(requiredMajorVersion, requiredMinorVersion, requiredRevision);
+
+var hpContact = {
+
+	/** Width of content box gallery viewer */
+	imgWidth : 1200,
+	
+	/** Height of flash gallery viewer */
+	imgHeight : 800,
+
+	/** Ratio of width to height */
+	imgRatio : 0,
+
+	/** Minimum allowed width */
+	minWidth : 800,
+	
+	commandURL : "<?= PageManager::$theme_url_root?>php/SendEmail.php",
+	
+	page_id : <?=PageManager::$page_id?>,
+	
+	site_id : <?=PageManager::$site_id?>,
+        
+        nonce : '<?= SecurityUtils::createNonce('email-link') ?>',
+	
+	sentMessage : 'Message sent, thankyou!',
+	
+	sentErrorMessage : 'Error sending message!',
+	
+	init : function(){
+						
+		// Optimize size for gallery
+		//hpContact.imgRatio = hpContact.imgHeight / hpContact.imgWidth;
+		hpContact.imgRatio = hpContact.imgWidth / hpContact.imgHeight;
+	
+		hpContact.onResize();
+		
+		setTimeout("hpContact.onResize()", 200);
+			
+			
+		// Validation
+		$("#contactForm").validate();
+			
+			
+		$.validator.addMethod(
+			"required_email", function(value, element) { 
+			
+		  		if (value == 'Your E-mail Address') return false; 
+		  		if (value == '') return false; 
+		  		
+		  		// Check to see if this group is complete
+		  		return hpContact.checkEmail(value);
+			}, 
+			"Enter a valid email");
+
+		$.validator.addMethod(
+			"required_name", function(value, element) { 
+			
+		  		if (value == 'Your Name') return false; 
+		  		if (value == '') return false; 
+		  		
+		  		return true;
+			}, 
+			"Please enter your name");
+	
+					
+		// Setup date picker...
+		Date.firstDayOfWeek = 0;
+		Date.format = 'mm/dd/yyyy';
+		$('.datepicker').datePicker({clickInput:true, startDate:'01/01/2009'});
+								
+	},
+	
+	checkEmail : function(email) {
+		var filter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+		if (!filter.test(email)) {
+			return false;
+		}
+		return true;
+	},
+	
+	onResize : function(){
+
+		if (hasFlash){
+								
+			var galH = $("#wrapper").height() - $("#nav_container").height();
+			var galW = Math.floor(hpContact.imgRatio * galH);	
+					
+			//alert('Min width: ' + $("#nav_container").attr('min-width'));			
+			
+			if (galW > hpContact.minWidth){
+			
+				$("#nav_container").width(galW);	
+				$("#container").width(galW);	
+				
+				$("#content").height(galH);
+				$("#content").width(galW);
+				
+			}
+		}
+		
+	},
+	
+	onChange : function(){
+		$("#contactForm").valid();
+	},
+	
+	onSubmit : function(){
+		
+		if ($("#contactForm").valid()){
+			
+			var aName = $("#name").val();
+			var aEmail = $("#email").val();
+			var aPhone = $("#phone").val();
+			var aLocation = $("#location").val();
+			var aDatetime = $("#datetime").val();
+			var aComments = $("#comments").val();
+			
+			//alert(aName + " " + aEmail + " " + aPhone + " " + aLocation + " " + aDatetime + " " + aComments); 
+			
+			var paras = {
+					page_id: hpContact.page_id, 
+					site_id: hpContact.site_id, 
+					nonce: hpContact.nonce, 
+					name: aName, 
+					email: aEmail, 
+					phone: aPhone, 
+					location: aLocation, 
+					comments: aComments, 
+					datetime: aDatetime
+			};
+										
+			$.ajax({
+				url: hpContact.commandURL,
+				dataType: "text",
+				data: paras,
+				success: function(ret){hpContact.onSentForm(ret);}
+			});	
+		}	
+		
+		return false;
+	},
+		
+	onSentForm : function(ret){
+		
+		if (ret == "TRUE"){
+			$('.formMessage').html(hpContact.sentMessage);
+		}
+		else if (ret == "FALSE"){
+			$('.formMessage').html(hpContact.sentErrorMessage);
+		}
+		else if (ret == "SPAM"){
+			$('.formMessage').html("Sorry, this comment looks like spam?");
+		}
+		else if (ret == "NA"){
+			$('.formMessage').html("Not authorized error!");
+		}
+		else {
+			$('.formMessage').html("Unknown error!");
+		}
+	}
+
+}
+
+// /////////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(hpContact.init);
+$(window).resize(hpContact.onResize);
+
+</script>
---
+++ admin/themes/Thebe/php/SendEmail.php
@@ -0,0 +1,107 @@
+<?php
+
+// Figure out the location of this file
+$discRoot = realpath(dirname(__FILE__)) . "/";
+$codeRoot = substr($discRoot, 0, strpos($discRoot, "themes")) . '/code/php/';
+$themeRoot = substr($discRoot, 0, strpos($discRoot, "php"));
+
+require_once($codeRoot . 'setup.php');
+
+// Get the users email.....
+
+$page_id = CommandHelper::getPara('page_id', true, CommandHelper::$PARA_TYPE_NUMERIC);
+$site_id = CommandHelper::getPara('site_id', true, CommandHelper::$PARA_TYPE_NUMERIC);
+$nonce = CommandHelper::getPara('nonce', true, CommandHelper::$PARA_TYPE_STRING);
+
+// Verify this request came from a real page
+if (!SecurityUtils::checkNonce($nonce, 'email-link')) {
+    Logger::error("Not authorized!");
+    echo "NA";
+    die();
+}
+
+// Get the form paras....
+$name = CommandHelper::getPara('name', false, CommandHelper::$PARA_TYPE_STRING);
+$client_email = CommandHelper::getPara('email', false, CommandHelper::$PARA_TYPE_STRING);
+$phone = CommandHelper::getPara('phone', false, CommandHelper::$PARA_TYPE_STRING);
+$location = CommandHelper::getPara('location', false, CommandHelper::$PARA_TYPE_STRING);
+$datetime = CommandHelper::getPara('datetime', false, CommandHelper::$PARA_TYPE_STRING);
+$comments = CommandHelper::getPara('comments', false, CommandHelper::$PARA_TYPE_STRING);
+
+// Check to see if the content is spam!
+$blogURL = PageManager::getPageLink($page_id);
+
+$author_ip = PageViewsTable::getRealIPAddr();
+$author_url = '';
+
+$akismet = new Akismet($blogURL , AKISMET_API_KEY);
+$akismet->setCommentAuthor($name);
+$akismet->setCommentAuthorEmail($client_email);
+$akismet->setCommentAuthorURL($author_url);
+$akismet->setCommentContent($comments);
+
+// Check from akismet..
+if($akismet->isCommentSpam()){
+    Logger::error("Caught some spam!");
+    echo "SPAM";
+    die();
+}
+
+
+//
+// Check to see if this is possible spam or not?
+//
+
+// Innocent until proven guilty...
+$status = 'Pending';
+$isSpam = false;
+
+// Check from akismet..
+if($akismet->isCommentSpam()){
+    $status = 'Spam';
+    $isSpam = true;
+}
+
+
+
+// Construct email
+$headers = "From: " . $client_email;
+$headers = "From: $client_email\r\nX-Mailer: php";
+//$replyto = $client_email;
+
+$subject = "Customer Request";
+
+$message = "Message from contact form;\n";
+$message .= "\n";
+$message .= "Phone: $phone\n";
+$message .= "\n";
+$message .= "Location: $location\n";
+$message .= "\n";
+$message .= "Date: $datetime\n";
+$message .= "\n";
+$message .= "Comments: $comments\n";
+
+// Get the owner id(s)for this site
+$user_list = UserTable::getUsersFromSiteID($site_id);
+
+foreach($user_list as $user){
+
+    Logger::dump($user);
+
+    $target_email = $user['email'];
+    Logger::debug("Target email: $target_email");
+
+    //Logger::debug("mail($target_email, $subject, $message, $headers)");
+
+    if (mail($target_email, $subject, $message, $headers)) {
+        Logger::debug("Email sent OK!");
+        echo "TRUE";
+    } else {
+        Logger::error("Error sending email");
+        echo "FALSE";
+    }
+
+}
+
+
+?>
