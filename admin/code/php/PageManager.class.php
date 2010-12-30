33fbe0e53fd5cf8ac837d5dc23866038e247ddd3
Mike Pritchard <mike@adastrasystems.com>
2010-12-29 21:23:30 -0500

    google tracker bug

--- admin/code/php/PageManager.class.php
+++ admin/code/php/PageManager.class.php
@@ -427,24 +427,28 @@ class PageManager {
 		
 		</script>
 		
-		
-		
-		<script type="text/javascript">
-		
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-20462178-1']);
-		  _gaq.push(['_setDomainName', '.apollosites.com']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		
-		</script>		
-	
 		*/
+		
+
+        if (isset($tracker_code) && $tracker_code != ''){        
+			echo "
+			<script type='text/javascript'>
+			
+			  var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', '$tracker_code']);
+			  _gaq.push(['_setDomainName', '.apollosites.com']);
+			  _gaq.push(['_trackPageview']);
+			
+			  (function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+			
+			</script>";
+		}
+				
+	/*
         if (isset($tracker_code) && $tracker_code != ''){        
 			echo "<!-- Global tracking --> \n";
 			echo "<script type='text/javascript'> \n";
@@ -459,7 +463,7 @@ class PageManager {
 			echo "} catch(err) {} \n";
 			echo "</script> \n";
 		}
-        
+    */    
     }
 
     // ///////////////////////////////////////////////////////////////////////////////////////
