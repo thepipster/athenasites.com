4b509df9d13d7e440efde2f8968e41591e5f6764
Mike Pritchard <mike@adastrasystems.com>
2011-01-18 03:30:56 -0500

    needed to cross reference theme para id

--- admin/code/php/dal/ClientGalleryTable.class.php
+++ admin/code/php/dal/ClientGalleryTable.class.php
@@ -13,7 +13,16 @@ class ClientGalleryTable {
 	*/
 	public static function getImagesForPage($site_id, $page_id){
 		//Logger::debug("getImagesForPage($site_id, $page_id)");
-		$sql = DatabaseManager::prepare("SELECT * FROM athena_%d_GalleryTable WHERE page_id = %d ORDER BY slot_number ASC",  $site_id, $page_id); 		
+		$sql = DatabaseManager::prepare("SELECT * FROM athena_%d_GalleryTable gt
+			INNER JOIN athena_%d_Pages pg
+			INNER JOIN apollo_ThemeParas tp
+			WHERE gt.theme_para_id = tp.id
+			AND pg.template = tp.page_template_name
+			AND pg.id = gt.page_id
+			AND gt.page_id = %d 
+			ORDER BY gt.slot_number ASC", $site_id, $site_id, $page_id); 	
+			
+		Logger::debug($sql);	
 		return DatabaseManager::getResults($sql);		
 	}
 
